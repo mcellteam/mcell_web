e89a1f97f7f79152b2ceacd2a28bb415f6e2e171
Bob Kuczewski <cnlbob@users.noreply.github.com>
2016-09-22 17:17:00 -0700

    Initial version of files for this project.

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<p>The (internal) home of mcell.</p>
+
+
+<?php phpinfo(); ?>
+
+<?php echo '<p>Hello World</p>'; ?>
+
+
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<p>The (internal) home of mcell.</p>
+
+
+<?php phpinfo(); ?>
+
+
+
+</body>
+</html>
