763031c7bfb93137e152d99d54eecd5fa329c244
Bob Kuczewski <cnlbob@users.noreply.github.com>
2016-09-23 23:22:29 -0700

    Added start and end seeds and handled output for multiple seeds.

--- test_code/run_mcell.php
+++ test_code/run_mcell.php
@@ -7,12 +7,32 @@
 
 <body>
 
-<style>
+<style type="text/css">
+/* From phpinfo style */
+body {background-color: #fff; color: #222; font-family: sans-serif;}
+pre {margin: 0; font-family: monospace;}
+a:link {color: #009; text-decoration: none; background-color: #fff;}
+a:hover {text-decoration: underline;}
+table {border-collapse: collapse; border: 0; width: 934px; box-shadow: 1px 2px 3px #ccc;}
+.center {text-align: center;}
+.center table {margin: 1em auto; text-align: left;}
+.center th {text-align: center !important;}
+td, th {border: 1px solid #666; font-size: 75%; vertical-align: baseline; padding: 4px 5px;}
+h1 {font-size: 150%;}
+h2 {font-size: 125%;}
+.p {text-align: left;}
+.e {background-color: #ccf; width: 300px; font-weight: bold;}
+.h {background-color: #99c; font-weight: bold;}
+.v {background-color: #ddd; max-width: 300px; overflow-x: auto;}
+.v i {color: #999;}
+img {float: right; border: 0;}
+hr {width: 934px; background-color: #ccc; border: 0; height: 1px;}
 
+/* Additional styles */
 /* Body for the entire document */
 body {
-  background-color: #eeeeee;
-  color: #000066;
+  background-color: #ffffff;
+  color: #000033;
   margin-left: 10px;
   margin-right: 10px;
   margin-top: 10px;
@@ -21,9 +41,12 @@ body {
 
 table, th, td {
   border: 1px solid black;
-  border-collapse: collapse;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  /* border-collapse: collapse; */
 }
-
 </style>
 
 
@@ -32,42 +55,72 @@ table, th, td {
 <hr/>
 
 <form action="run_mcell.php" method="post">
+
 <?php
+
 $model_name = "";
 if (in_array("model_name",array_keys($_POST))) {
   $model_name = $_POST["model_name"];
 }
-$seed = 1;
-if (in_array("seed",array_keys($_POST))) {
-  $seed = $_POST["seed"];
+$start_seed = 1;
+if (in_array("start_seed",array_keys($_POST))) {
+  $start_seed = $_POST["start_seed"];
+}
+$end_seed = 1;
+if (in_array("end_seed",array_keys($_POST))) {
+  $end_seed = $_POST["end_seed"];
 }
 $what = "";
 if (in_array("what",array_keys($_POST))) {
   $what = $_POST["what"];
 }
 
+if ($start_seed < 1) {
+  $start_seed = 1;
+}
+if ($end_seed > 20) {
+  $end_seed = 20;
+}
+if ($end_seed < $start_seed) {
+  $end_seed = $start_seed;
+}
+
 echo "<b>Model Name:</b> &nbsp; <input type=\"text\" name=\"model_name\" value=".$model_name.">";
-echo " &nbsp; &nbsp; <b>Seed:</b> &nbsp; <input type=\"text\" name=\"seed\" value=".$seed.">";
-// echo " &nbsp; &nbsp; <input type=\"submit\" value=\"Run MCell\">";
+echo " &nbsp; &nbsp; <b>Seeds:</b> &nbsp; <input type=\"text\" size=\"4\" min=\"1\" max=\"2000\" name=\"start_seed\" value=".$start_seed.">";
+echo " &nbsp; to &nbsp;                   <input type=\"text\" size=\"4\" min=\"1\" max=\"2000\" name=\"end_seed\" value=".$end_seed.">";
 echo " &nbsp; &nbsp; <button type=\"submit\" name=\"what\" value=\"run\">Run MCell</button>";
 echo " &nbsp; &nbsp; <button type=\"submit\" name=\"what\" value=\"clear\">Clear</button>";
 $output = "";
 if (strlen($what) > 0) {
+  $sep = "=======================================================================================";
   if (strcmp($what,"clear") == 0) {
-    $output = shell_exec ("rm -Rf viz_data; ls -lR");
+    $output = "\n\n".$sep."\n  Directory Listing After Clear \n".$sep."\n\n".shell_exec ("rm -Rf viz_data; ls -lR");
   } elseif (strcmp($what,"run") == 0) {
     if (strlen($model_name) > 0) {
       //$result = popen("/bin/ls", "r");
-      echo "<br/><b>MCell output ...</b>";
-      $sep = "=======================================================================================";
-      $output = shell_exec ("./mcell -seed ".$seed." ".$model_name."; echo \" \"; echo ".$sep."; echo \" \"; ls -lR;");
+      echo "<br/><p><b>MCell output:</b></p>";
+      $output = "";
+      $result = "";
+      for ($seed = $start_seed; $seed <= $end_seed; $seed++) {
+        $result = shell_exec ("./mcell -seed ".$seed." ".$model_name."; echo \" \"; echo ".$sep."; echo \" \";");
+        if ($seed == $start_seed) {
+          $output = $result;
+        } else {
+          $output = $output."\n\n".$sep."\n\n".$result;
+        }
+      }
+      $result = shell_exec ("ls -lR;");
+      $output = $output."\n\n".$sep."\n  Directory Listing After All Runs \n".$sep."\n\n".$result;
     }
   }
 }
-echo "<div class=\"center\"><table><tr><td><pre>$output</pre></td></tr></table></div>";
+echo "<center><table><tr><td><pre>$output</pre></td></tr></table></center>";
+
 ?>
+
 </form>
 
+<!--
 <?php
 echo "<div class=\"center\"><h2>PHP GLOBALS</h2><table>";
 echo "<tr class=\"h\"><th colspan=\"2\">Global Variables from \"foreach\"</th></tr>";
@@ -80,9 +133,11 @@ foreach ( $GLOBALS as $gl_key => $gl_val ) {
   echo "</tr>";
 }
 echo "</table></div>";
-?>  
+?>
 
 <?php phpinfo(); ?>
+
+-->
 
 </body>
 </html>
