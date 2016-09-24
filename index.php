6801d34dab3491bffe53cc387f8ae4e39bff13fb
Bob Kuczewski <cnlbob@users.noreply.github.com>
2016-09-23 17:32:25 -0700

    Added some more files for the web development project.

--- index.php
+++ index.php
@@ -1,12 +1,90 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 1.0 Transitional//EN">
 <html>
+
+<head>
+<title>MCell Web Development Home</title>
+<link rel="stylesheet" type="text/css" href="style/def.css" />
+</head>
+
 <body>
-<p>The (internal) home of mcell.</p>
+
+<div class="zeromargins">
+<center><img class="zeromargins" src="share/rat_nmj.jpg" width="100%"></center>
+</div>
+
+<table class="zeromargins" width="100%" border="0">
+ <colgroup>
+  <col width="20%" />
+  <col />
+ </colgroup>
+ <tr valign="top">
+  <td class="leftside">
+   <p class="leftside"><a href="test_code/data_model_testing.htm">Data Model Browser</a></p>
+   <p class="leftside"><a href="mysql-edit.php">Database Administration</a></p>
+   <p class="leftside"><a href="test_code/php_show_info.php">PHP Information</a></p>
+  </td>
+  <td class="rightside">
 
 
-<?php phpinfo(); ?>
+<div class="rightside">
+<!----------------------------------------------------------------->
 
-<?php echo '<p>Hello World</p>'; ?>
+<h1>MCell Web Development at mcell.snl.salk.edu</h1>
+<p class="indent1">
+This site is being used for MCell Web Development.
+</p>
+<p class="indent1">
+Projects include:
+</p>
+<ul>
+<li>A local Galaxy Server</li>
+<li>Tools for running MCell via the web</li>
+</ul>
+<p class="indent1">
+These are just a start.
+</p>
+<hr />
+<h2>History and Current Status (September 2016)</h2>
+<p class="indent1">
+Currently running a LAMP server...
+</p>
 
+<!----------------------------------------------------------------->
+<!-- This is just a way of extending the main page so it fills the window vertically -->
+<pre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</pre>
+</div>
+
+<tr><td></td></tr>
+
+</table>
 
 </body>
+
+
 </html>
---
+++ test_code/php_show_info.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php echo '<center><h1>MCell Web Development at mcell.snl.salk.edu</h1></center>'; ?>
+
+<hr/>
+
+<?php phpinfo(); ?>
+
+
+
+</body>
+</html>
