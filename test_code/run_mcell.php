e921bf1dfa4e19cbcf8db5600e6802400b128e46
Bob Kuczewski <cnlbob@users.noreply.github.com>
2016-09-23 22:28:50 -0700

    Added a "clear" button to remove the old runs.

--- test_code/run_mcell.php
+++ test_code/run_mcell.php
@@ -41,17 +41,30 @@ $seed = 1;
 if (in_array("seed",array_keys($_POST))) {
   $seed = $_POST["seed"];
 }
+$what = "";
+if (in_array("what",array_keys($_POST))) {
+  $what = $_POST["what"];
+}
+
 echo "<b>Model Name:</b> &nbsp; <input type=\"text\" name=\"model_name\" value=".$model_name.">";
 echo " &nbsp; &nbsp; <b>Seed:</b> &nbsp; <input type=\"text\" name=\"seed\" value=".$seed.">";
-echo " &nbsp; &nbsp; <input type=\"submit\" value=\"Run MCell\">";
-if (strlen($model_name) > 0) {
-  //$result = popen("/bin/ls", "r");
-  echo "<br/><b>MCell output ...</b>";
-  $sep = "=======================================================================================";
-  $output = shell_exec ("ls -lR ..; echo \" \"; echo ".$sep."; echo \" \"; ./mcell -seed ".$seed." Scene.main.mdl");
-  //$output = shell_exec("./mcell -seed ".$seed." Scene.main.mdl");
-  echo "<div class=\"center\"><table><tr><td><pre>$output</pre></td></tr></table></div>";
+// echo " &nbsp; &nbsp; <input type=\"submit\" value=\"Run MCell\">";
+echo " &nbsp; &nbsp; <button type=\"submit\" name=\"what\" value=\"run\">Run MCell</button>";
+echo " &nbsp; &nbsp; <button type=\"submit\" name=\"what\" value=\"clear\">Clear</button>";
+$output = "";
+if (strlen($what) > 0) {
+  if (strcmp($what,"clear") == 0) {
+    $output = shell_exec ("rm -Rf viz_data; ls -lR");
+  } elseif (strcmp($what,"run") == 0) {
+    if (strlen($model_name) > 0) {
+      //$result = popen("/bin/ls", "r");
+      echo "<br/><b>MCell output ...</b>";
+      $sep = "=======================================================================================";
+      $output = shell_exec ("./mcell -seed ".$seed." ".$model_name."; echo \" \"; echo ".$sep."; echo \" \"; ls -lR;");
+    }
+  }
 }
+echo "<div class=\"center\"><table><tr><td><pre>$output</pre></td></tr></table></div>";
 ?>
 </form>
 
