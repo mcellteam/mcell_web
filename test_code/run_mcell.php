deb7582253b2600bb2639cd0c5f25b137034e170
Bob Kuczewski <cnlbob@users.noreply.github.com>
2016-09-26 18:55:18 -0700

    Added the ability to select a model and a number of other small changes.

--- test_code/run_mcell.php
+++ test_code/run_mcell.php
@@ -7,6 +7,9 @@
 
 <body>
 
+<a href="../.."> <b>Home</b> </a>
+<hr/>
+
 <style type="text/css">
 /* From phpinfo style */
 body {background-color: #fff; color: #222; font-family: sans-serif;}
@@ -58,9 +61,15 @@ table, th, td {
 
 <?php
 
-$model_name = "";
-if (in_array("model_name",array_keys($_POST))) {
-  $model_name = $_POST["model_name"];
+echo "<center>";
+
+$model_file_name = "";
+if (in_array("model_file_name",array_keys($_POST))) {
+  $model_file_name = $_POST["model_file_name"];
+}
+$model_file = "";
+if (in_array("model_file",array_keys($_POST))) {
+  $model_file = $_POST["model_file"];
 }
 $start_seed = 1;
 if (in_array("start_seed",array_keys($_POST))) {
@@ -85,24 +94,37 @@ if ($end_seed < $start_seed) {
   $end_seed = $start_seed;
 }
 
-echo "<b>Model Name:</b> &nbsp; <input type=\"text\" name=\"model_name\" value=".$model_name.">";
-echo " &nbsp; &nbsp; <b>Seeds:</b> &nbsp; <input type=\"text\" size=\"4\" min=\"1\" max=\"2000\" name=\"start_seed\" value=".$start_seed.">";
-echo " &nbsp; to &nbsp;                   <input type=\"text\" size=\"4\" min=\"1\" max=\"2000\" name=\"end_seed\" value=".$end_seed.">";
-echo " &nbsp; &nbsp; <button type=\"submit\" name=\"what\" value=\"run\">Run MCell</button>";
-echo " &nbsp; &nbsp; <button type=\"submit\" name=\"what\" value=\"clear\">Clear</button>";
+$model_files = glob("*.mdl");
+
+echo "<b>Model Name:</b> &nbsp; <select name=\"model_file_name\">\n";
+echo "  <option value=\"\"></option>>";
+for ($model_file_index=0; $model_file_index<count($model_files); $model_file_index++) {
+  $sel = "";
+  if ($model_files[$model_file_index] == $model_file_name) {
+    $sel = " selected ";
+  }
+  echo "  <option ".$sel."value=".$model_files[$model_file_index].">".$model_files[$model_file_index]."</option>\n";
+}
+echo "</select>\n";
+
+//echo "<b>Model Name:</b> &nbsp; <input type=\"text\" name=\"model_name\" value=".$model_name.">";
+echo " &nbsp; &nbsp; <b>Seeds:</b> &nbsp; <input type=\"text\" size=\"4\" min=\"1\" max=\"2000\" name=\"start_seed\" value=".$start_seed.">\n";
+echo " &nbsp; to &nbsp;                   <input type=\"text\" size=\"4\" min=\"1\" max=\"2000\" name=\"end_seed\" value=".$end_seed.">\n";
+echo " &nbsp; &nbsp; <button type=\"submit\" name=\"what\" value=\"run\">Run MCell</button>\n";
+echo " &nbsp; &nbsp; <button type=\"submit\" name=\"what\" value=\"clear\">Clear</button>\n";
 $output = "";
 if (strlen($what) > 0) {
   $sep = "=======================================================================================";
   if (strcmp($what,"clear") == 0) {
     $output = "\n\n".$sep."\n  Directory Listing After Clear \n".$sep."\n\n".shell_exec ("rm -Rf viz_data; rm -Rf react_data; ls -lR");
   } elseif (strcmp($what,"run") == 0) {
-    if (strlen($model_name) > 0) {
+    if (strlen($model_file_name) > 0) {
       //$result = popen("/bin/ls", "r");
       echo "<br/><p><b>MCell output:</b></p>";
       $output = "";
       $result = "";
       for ($seed = $start_seed; $seed <= $end_seed; $seed++) {
-        $result = shell_exec ("./mcell -seed ".$seed." ".$model_name."; echo \" \"; echo ".$sep."; echo \" \";");
+        $result = shell_exec ("./mcell -seed ".$seed." ".$model_file_name."; echo \" \"; echo ".$sep."; echo \" \";");
         if ($seed == $start_seed) {
           $output = $result;
         } else {
@@ -114,89 +136,9 @@ if (strlen($what) > 0) {
     }
   }
 }
+echo "</center>";
+
 echo "<center><table><tr><td><pre>$output</pre></td></tr></table></center>";
-/*
-if (strlen($what) > 0) {
-  // Draw a plot
-  echo "<center><canvas id=\"drawing_area\" width=\"800\" height=\"500\" style=\"border:1px solid #d3d3d3;\">";
-  echo "Your browser does not support the HTML5 canvas tag.</canvas></center>";
-}
-*/
-
-/*
-
-$seed_folders = glob("react_data/*");
-
-$plot_data = array_fill ( 0, count($seed_folders), array ( array(0,1), array(0,1) ) );
-for ($seed_folder_index=0; $seed_folder_index<count($seed_folders); $seed_folder_index++) {
-  echo "Seed Folders = ".$seed_folders[$seed_folder_index]."<br/>";
-  $seed_files = glob($seed_folders[$seed_folder_index]."/*");
-  $plot_data[$seed_folder_index] = array_fill ( 0, count($seed_files), array(0,1) );
-  for ($seed_file_index=0; $seed_file_index<count($seed_files); $seed_file_index++) {
-    $seed_file_lines = explode ( "\n", file_get_contents ( $seed_files[$seed_file_index] ) );
-    $num_lines = count($seed_file_lines);
-
-
-
-    echo " &nbsp;  &nbsp;  &nbsp; Seed File contains ".$num_lines." lines.<br/>";
-    $plot_data[$seed_folder_index][$seed_file_index][0] = array_fill ( 0, $num_lines, 0 );
-    $plot_data[$seed_folder_index][$seed_file_index][1] = array_fill ( 0, $num_lines, 0 );
-    for ($seed_line_index=0; $seed_line_index<$num_lines; $seed_line_index++) {
-      // echo " &nbsp;  &nbsp;  &nbsp;  &nbsp; ".$seed_line_parts[0].", ".$seed_line_parts[1]."<br/>";
-      $seed_line_parts = explode ( " ", trim($seed_file_lines[$seed_line_index]) );
-      // Good here: echo " &nbsp;  &nbsp;  &nbsp;  &nbsp; ".$seed_line_parts[0].", ".$seed_line_parts[1]."<br/>";
-      $x = 0;
-      $y = 0;
-      if (count($seed_line_parts) == 2) {
-        $x = floatval(trim($seed_line_parts[0]));
-        $y = floatval(trim($seed_line_parts[1]));
-      }
-      $plot_data[$seed_folder_index][$seed_file_index][0][$seed_line_index] = $x;
-      $plot_data[$seed_folder_index][$seed_file_index][1][$seed_line_index] = $y;
-      echo " &nbsp;  &nbsp;  &nbsp;  &nbsp; ".$x.", ".$y."<br/>";
-    }
-  }
-}
-
-
-*/
-
-/*
-$plot_data = array ( 
-                 array (
-                     array(0.1,1.1,2.1,3.1,4.1,5.1,6.1,7.1),
-                     array(0.1,1.1,3.1,3.1,3.1,5.1,7.1,9.1)
-                 ),
-                 array (
-                     array(0.1,1.1,2.1,3.1,4.1,5.1,6.1,7.1),
-                     array(8.1,8.1,9.1,9.1,8.1,8.1,7.1,7.1)
-                 ),
-                 array (
-                     array(0.1,1.1,2.1,3.1,4.1,5.1,6.1,7.1),
-                     array(0.1,0.1,0.1,0.1,9.1,0.1,0.1,0.1)
-                 ),
-                 array (
-                     array(0.1,1.1,2.1,3.1,4.1,5.1,6.1,7.1),
-                     array(9.1,7.1,6.1,5.1,4.1,3.1,3.1,2.1)
-                 )
-             );
-*/
-
-$plot_data = array ();
-for ($i=0;$i<4;$i++) {
-  $plot_data[$i] = array();
-  for ($j=0;$j<2;$j++) {
-    $plot_data[$i][$j] = array();
-    for ($k=0;$k<8;$k++) {
-      $x = $k;
-      if ($j > 0) {
-        $x = (5*$i) + ($k * $k);
-      }
-      $plot_data[$i][$j][$k] = $x;
-    }
-  }
-}
-
 
 
 $plot_data = array();
@@ -204,7 +146,7 @@ $plot_file_num = 0;
 
 $seed_folders = glob("react_data/*");
 for ($seed_folder_index=0; $seed_folder_index<count($seed_folders); $seed_folder_index++) {
-  echo "Seed Folder = ".$seed_folders[$seed_folder_index]."<br/>";
+  // echo "Folder = \"".$seed_folders[$seed_folder_index]."\"<br/>";
 
   $mol_files = glob($seed_folders[$seed_folder_index]."/*");
 
@@ -217,14 +159,13 @@ for ($seed_folder_index=0; $seed_folder_index<count($seed_folders); $seed_folder
     $seed_file_lines = explode ( "\n", file_get_contents ( $mol_files[$mol_file_index] ) );
     $num_lines = count($seed_file_lines);
 
-    echo " &nbsp;  &nbsp;  &nbsp; Seed File contains ".$num_lines." lines.<br/>";
+    // echo " &nbsp;  &nbsp;  &nbsp; File \"".$mol_files[$mol_file_index]."\" contains ".$num_lines." lines.<br/>";
 
     $plot_line_num = 0;
     for ($seed_line_index=0; $seed_line_index<$num_lines; $seed_line_index++) {
 
-      // echo " &nbsp;  &nbsp;  &nbsp;  &nbsp; ".$seed_line_parts[0].", ".$seed_line_parts[1]."<br/>";
       $seed_line_parts = explode ( " ", trim($seed_file_lines[$seed_line_index]) );
-      // echo " &nbsp;  &nbsp;  &nbsp;  &nbsp; ".$seed_line_parts[0].", ".$seed_line_parts[1]."<br/>";
+
       if (count($seed_line_parts) == 2) {
         $x = floatval(trim($seed_line_parts[0]));
         $y = floatval(trim($seed_line_parts[1]));
@@ -232,7 +173,7 @@ for ($seed_folder_index=0; $seed_folder_index<count($seed_folders); $seed_folder
         $plot_data[$plot_file_num][1][$plot_line_num] = $y;
         $plot_line_num = $plot_line_num + 1;
       }
-      // echo " &nbsp;  &nbsp;  &nbsp;  &nbsp; ".$x.", ".$y."<br/>";
+
     }
 
     $plot_file_num = $plot_file_num + 1;
@@ -240,22 +181,7 @@ for ($seed_folder_index=0; $seed_folder_index<count($seed_folders); $seed_folder
   }
 }
 
-
-// Dump the plot data
-
-for ($ver1=0; $ver1<count($plot_data); $ver1++) {
-  echo "<br/>";
-  for ($ver2=0; $ver2<count($plot_data[$ver1]); $ver2++) {
-    echo "<br/>";
-    for ($ver3=0; $ver3<count($plot_data[$ver1][$ver2]); $ver3++) {
-      $coord = "x";
-      if ($ver2 != 0) {
-        $coord = "y";
-      }
-      echo " &nbsp;  &nbsp; ".$coord." [".$ver1."][".$ver2."][".$ver3."] = ".$plot_data[$ver1][$ver2][$ver3]."<br/>";
-    }
-  }
-}
+// phpinfo();
 
 ?>
 
@@ -266,16 +192,20 @@ for ($ver1=0; $ver1<count($plot_data); $ver1++) {
 
 <h1>Count Output Plot</h1>
 
-<canvas id="drawing_area" width="800" height="500" style="border:1px solid #d3d3d3;">
+<canvas id="drawing_area" width="800" height="600" style="border:1px solid #d3d3d3;">
 Your browser does not support the HTML5 canvas tag.</canvas>
 <p/>
 </center>
 
 <script>
 
+// PHP will have filled in the $plot_data variable with the plot data (above).
+// This next line generates an in-line JavaScript statement containing all of that data.
+// The resulting 3D array will be indexed by Plot#, x/y, step
+
 var plot_data = <?php echo json_encode($plot_data); ?>;
 
-// console.log ( plot_data );
+// This function just draws a plot of plot_data
 
 function draw_data() {
 
@@ -331,26 +261,6 @@ function draw_data() {
 draw_data();
 
 </script>
-
-
-<!--
-<?php
-echo "<div class=\"center\"><h2>PHP GLOBALS</h2><table>";
-echo "<tr class=\"h\"><th colspan=\"2\">Global Variables from \"foreach\"</th></tr>";
-echo "<tr class=\"h\"><th>Key</th><th>Value</th></tr>";
-foreach ( $GLOBALS as $gl_key => $gl_val ) {
-  echo "<tr>";
-  echo "<td class=\"e\">" . $gl_key . "</td><td class=\"v\">";
-  var_dump ( $gl_val );
-  echo "</td>";
-  echo "</tr>";
-}
-echo "</table></div>";
-?>
-
-<?php phpinfo(); ?>
-
--->
 
 </body>
 </html>
